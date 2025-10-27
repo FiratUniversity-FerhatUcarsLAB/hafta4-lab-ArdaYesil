4afd8e657c49498a12caca2467ac990ce1794ca3
ArdaYesil <250542013@firat.edu.tr>
2025-10-27 17:01:18 +0300

    Update GeometrikHesap.java

--- GeometrikHesap.java
+++ GeometrikHesap.java
@@ -1,1 +1,35 @@
 
+import java.util.Scanner;
+
+class Main {
+  public static void main(String[] args) {
+    Scanner input = new Scanner(System.in);
+  
+      float pi = 3.14159f; // pi tanımlandı
+      Double alan, çevre, çap, yuzeyAlanı,hacim; //İstenilen Geometrik hesaplamaların değişkenlerini tanımladık
+
+      System.out.println("===== GEOMETRIK HESAPLAYICI =====");
+      System.out.print("Dairenin yarıcapını giriniz (cm):");
+
+      //Geometrik Hesaplamaları yapıldı
+      alan = pi * yarıcap * yarıcap;
+      çevre = 2 * pi * yarıcap;
+      çap = 2 * yarıcap;
+      yuzeyAlanı = 4 * pi * yarıcap * yarıcap;
+      hacim = 4.0*pi*yarıcap*yarıcap*yarıcap/3.0;
+
+
+      //Çıktı Gösterildi
+      System.out.println("\nSonuçlar :");
+      System.out.println("===========");
+      System.out.printf(" Daire Alanı       : %.2f", alan);
+      System.out.printf("\n Daire Çevresi     : %.2f", çevre);
+      System.out.printf("\n Daire Çapı        : %.2f", çap);
+      System.out.printf("\n Daire Hacmi       : %.2f", hacim);
+      System.out.printf("\n Daire Yüzey Alanı : %.2f", yuzeyAlanı);
+  
+    //Scanner kapandı
+    input.close();
+  }
+
+}
