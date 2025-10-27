1b71e6791218d9919cf7c6c80571dbe25d8d8e78
ArdaYesil <250542013@firat.edu.tr>
2025-10-27 17:40:19 +0300

    Update GeometrikHesap.java

--- GeometrikHesap.java
+++ GeometrikHesap.java
@@ -22,11 +22,11 @@ class Main {
       //Çıktı Gösterildi
       System.out.println("\nSonuçlar :");
       System.out.println("===========");
-      System.out.printf(" Daire Alanı       : %.2f", alan);
-      System.out.printf("\n Daire Çevresi     : %.2f", çevre);
-      System.out.printf("\n Daire Çapı        : %.2f", çap);
-      System.out.printf("\n Daire Hacmi       : %.2f", hacim);
-      System.out.printf("\n Daire Yüzey Alanı : %.2f", yuzeyAlanı);
+      System.out.printf(" Daire Alanı       : %.2f cm", alan);
+      System.out.printf("\n Daire Çevresi     : %.2f cm", çevre);
+      System.out.printf("\n Daire Çapı        : %.2f cm", çap);
+      System.out.printf("\n Daire Hacmi       : %.2f cm³", hacim);
+      System.out.printf("\n Daire Yüzey Alanı : %.2f cm", yuzeyAlanı);
   
     //Scanner kapandı
     input.close();
